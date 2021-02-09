0a48b012f6f14489ccd41f5f0c3cb2dc605d10c3
Alex <alexanderbraun@freenet.de>
2021-02-09 23:56:47 +0100

    module 4 done

---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { EventService } from './shared/event.service';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'events-list',
+  template: `
+    <div>
+      <h1>Upcoming Angular Events</h1>
+      <hr />
+      <div class="row">
+        <div class="col-md-5" *ngFor="let event of events">
+          <event-thumbnail
+            (click)="handleThumbnailClick(event.name)"
+            [event]="event"
+          ></event-thumbnail>
+        </div>
+      </div>
+    </div>
+  `,
+})
+export class EventsListComponent implements OnInit {
+  events: any;
+
+  constructor(
+    private eventService: EventService,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit() {
+    this.events = this.eventService.getEvents();
+  }
+
+  handleThumbnailClick(name: string, title?: string) {
+    this.toastr.success(name, title);
+  }
+}
